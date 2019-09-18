6e80d8aca7fc6a7a3261836317c828eb4dd65814
israelmarmar <email.israel.br@gmail.com>
2019-09-18 15:23:54 -0400

    fix bugs

--- ParseServer.js
+++ ParseServer.js
@@ -10,7 +10,7 @@ var app = express();var api = new ParseServer({
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://0.0.0.0:4040');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
